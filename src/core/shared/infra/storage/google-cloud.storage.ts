dcee797adfa344688ccc6ed2c902ed8be0f9ef25
douglastenfen <douglastenfen@gmail.com>
2025-02-10 23:40:25 +0000

    feat: add Google Cloud Storage integration

---
+++ src/core/shared/infra/storage/google-cloud.storage.ts
@@ -0,0 +1,37 @@
+import { IStorage } from '@core/shared/application/sotarge.interface';
+import { Storage as GoogleCloudStorageSdk } from '@google-cloud/storage';
+
+export class GoogleCloudStorage implements IStorage {
+  constructor(
+    private storageSdk: GoogleCloudStorageSdk,
+    private bucketName: string,
+  ) {}
+
+  store(object: {
+    data: Buffer;
+    mimeType?: string;
+    id: string;
+  }): Promise<void> {
+    const bucket = this.storageSdk.bucket(this.bucketName);
+    const file = bucket.file(object.id);
+    return file.save(object.data, {
+      metadata: {
+        contentType: object.mimeType,
+      },
+    });
+  }
+
+  async get(id: string): Promise<{ data: Buffer; mimeType?: string }> {
+    const file = this.storageSdk.bucket(this.bucketName).file(id);
+
+    const [metadata, content] = await Promise.all([
+      file.getMetadata(),
+      file.download(),
+    ]);
+
+    return {
+      data: content[0],
+      mimeType: metadata[0].contentType,
+    };
+  }
+}
