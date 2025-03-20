c21a0f6ee1d7b986b0fea10f67264506333bf8fc
douglastenfen <douglastenfen@gmail.com>
2025-03-20 20:30:31 -0300

    finishing rules for completed video upload

--- src/nest-modules/videos-module/videos.consumers.ts
+++ src/nest-modules/videos-module/videos.consumers.ts
@@ -1,20 +1,53 @@
+import { AudioVideoMediaStatus } from '@core/shared/domain/value-objects/audio-video-media.vo';
+import { ProcessAudioVideoMediaInput } from '@core/video/application/use-cases/process-audio-video-medias/process-audio-video-media.input';
+import { ProcessAudioVideoMediaUseCase } from '@core/video/application/use-cases/process-audio-video-medias/process-audio-video-media.use-case';
 import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
-import { Injectable } from '@nestjs/common';
+import { Injectable, ValidationPipe } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
 
 @Injectable()
 export class VideosConsumers {
+  constructor(private moduleRef: ModuleRef) {}
+
   @RabbitSubscribe({
     exchange: 'amq.direct',
     routingKey: 'video.convert',
     queue: 'micro-videos/admin',
+    allowNonJsonMessages: true,
   })
-  onProcessVideo(msg: {
+  async onProcessVideo(msg: {
     video: {
       resourceId: string;
       encodedVideoFolder: string;
       status: 'COMPLETED' | 'FAILED';
     };
   }) {
-    console.log(msg);
+    const resourceId = `${msg.video?.resourceId}`;
+
+    const [videoId, field] = resourceId.split('.');
+
+    const input = new ProcessAudioVideoMediaInput({
+      videoId,
+      field: field as 'trailer' | 'video',
+      encodedLocation: msg.video?.encodedVideoFolder,
+      status: msg.video?.status as AudioVideoMediaStatus,
+    });
+
+    try {
+      await new ValidationPipe({
+        errorHttpStatusCode: 422,
+      }).transform(input, {
+        metatype: ProcessAudioVideoMediaInput,
+        type: 'body',
+      });
+
+      const useCase = await this.moduleRef.resolve(
+        ProcessAudioVideoMediaUseCase,
+      );
+
+      await useCase.execute(input);
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
